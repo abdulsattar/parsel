082015f2f37fc7221686a68dea8b69668de4ba0f
Abdulsattar Mohammed <asattar.md@gmail.com>
2014-03-08 18:17:29 +0530

    Initialized

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('parselApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
